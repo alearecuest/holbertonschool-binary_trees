d23a0cc03c8a615f4603ce40fe2750a6e5966581
MaximoDilacio <developermaximo1@gmail.com>
2025-04-09 17:55:04 +0000

    Fix betty file 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,18 +7,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height = 0, right_height = 0;
+	size_t left_height = 0, right_height = 0;
 
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    if (left_height > right_height)
-        return left_height + 1;
-    else
-        return right_height + 1;
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
 
 /**
@@ -29,13 +29,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    size_t left_height = 0, right_height = 0;
+	size_t left_height = 0, right_height = 0;
 
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return (int)(left_height - right_height);
+	return ((int)(left_height - right_height));
 }
