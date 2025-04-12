dad64be511830c91e9db4db18c821394d447351e
padredemellis <10656@holbertonstudents.com>
2025-04-12 16:32:09 +0000

    C - Binary Trees18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,22 @@
 #include "binary_trees.h"
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/**
+ * binary_tree_sibling - Encuentra el hermano de un nodo en el árbol
+ * @node: Puntero al nodo objetivo
+ *
+ * Return: Puntero al nodo hermano o NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    if (!node || !node->parent)
+        return (NULL);
+
+    if (node == node->parent->left)
+        return (node->parent->right);
+    else
+        return (node->parent->left);
+}
+
 /**
  * binary_tree_uncle - Encuentra el tío de un nodo
  * @node: Puntero al nodo para encontrar el tío
@@ -8,8 +25,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node && node->parent)
-		return (binary_tree_sibling(node->parent));
-	return (NULL);
+    if (node && node->parent)
+        return (binary_tree_sibling(node->parent));
+    return (NULL);
 }
 
